a679ab2340434bb93b64c91734607de5c81199d4
Alissa Reynolds <alissa6710@gmail.com>
2023-11-29 11:09:26 -0600

    finished part 1 of movie project

---
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,17 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MovieController {
+    public static void main(String[] args) {
+
+//        @RequestMapping("/")
+//        public String getIndexPage()
+//        {
+//            return "index";
+//        }
+    }
+
+}
