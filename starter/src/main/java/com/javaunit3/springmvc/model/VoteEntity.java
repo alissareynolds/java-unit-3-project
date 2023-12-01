fb8f48929cf64f883a0b4a34ee16a4b2e4240d05
Alissa Reynolds <alissa6710@gmail.com>
2023-12-01 09:53:35 -0600

    finished part 4 of project

---
+++ starter/src/main/java/com/javaunit3/springmvc/model/VoteEntity.java
@@ -0,0 +1,34 @@
+package com.javaunit3.springmvc.model;
+
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "votes")
+public class VoteEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "voter_Name")
+    private String voterName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getVoterName() {
+        return voterName;
+    }
+
+    public void setVoterName(String voterName) {
+        this.voterName = voterName;
+    }
+}
