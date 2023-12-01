18b71a65344aa7b32fc8ef962ade1d24e41a5828
Alissa Reynolds <alissa6710@gmail.com>
2023-12-01 09:20:23 -0600

    finished part 2 and working on getting supabase set up

--- starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,17 +1,46 @@
 package com.javaunit3.springmvc;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MovieController {
-    public static void main(String[] args) {
 
-//        @RequestMapping("/")
-//        public String getIndexPage()
-//        {
-//            return "index";
-//        }
+    @Autowired
+    private BestMovieService bestMovieService;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+
+        String movieTitle = request.getParameter("movieTitle");
+
+        model.addAttribute("BestMovieVote", movieTitle);
+
+        return "voteForBestMovie";
     }
 
 }
